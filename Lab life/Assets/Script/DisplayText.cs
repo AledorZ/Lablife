bb435f9cad048a4283949f90217b9765b4db1e1c
Aledor <mohdfitri93@hmail.com>
2021-04-28 06:58:47 +0800

    Add project

---
+++ Lab life/Assets/Script/DisplayText.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayText : MonoBehaviour
+{
+    public string Mystring;
+    public Text MyText;
+    public float fadetime;
+    public bool displayinfo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyText = GameObject.Find("Text").GetComponent<Text>();
+        MyText.color = Color.clear;
+    }
+
+    // Update is called once per frame
+    void Update() => FadeText();
+    void OnMouseOver()
+    {
+        displayinfo = true;
+    }
+    void OnMouseExit()
+    {
+        displayinfo = false;
+    }
+
+    void FadeText()
+    {
+        if (displayinfo)
+        {
+            MyText.text = Mystring;
+            MyText.color = Color.Lerp(MyText.color, Color.white, fadetime * Time.deltaTime);
+        }
+        else
+        {
+            MyText.color = Color.Lerp(MyText.color, Color.clear, fadetime * Time.deltaTime);
+        }
+    }
+    
+
+    
+}
