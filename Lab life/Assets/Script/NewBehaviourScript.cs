47e0e7467e5a22065f0b02d7bdfab8995c2ed94c
Aledor <mohdfitri93@hmail.com>
2021-06-03 00:26:58 +0800

    Camera switch
    New script

---
+++ Lab life/Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject camera1, camera2;
+   
+
+    // Update is called once per frame
+    void Update()
+
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            camera1.SetActive(false);
+            camera2.SetActive(true);
+            
+            gameObject.GetComponent<NewBehaviourScript>().enabled = false;
+
+            gameObject.GetComponent<cameraswitch>().enabled = true;
+        }
+        
+
+    }
+}
---
+++ Lab life/Assets/Script/cameraswitch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraswitch : MonoBehaviour
+    
+{
+    public GameObject camera1, camera2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            camera1.SetActive(true);
+            camera2.SetActive(false);
+           
+            gameObject.GetComponent<NewBehaviourScript>().enabled = true;
+
+            gameObject.GetComponent<cameraswitch>().enabled = false;
+        }
+        
+
+    }
+}
